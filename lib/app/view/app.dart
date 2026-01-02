517b65694dc76ea2179929b01109e31dcf5767d4
Cihat Karaboğa <hestu12@gmail.com>
2026-01-03 02:49:01 +0300

    refactor: update app view

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:azteron_case/core/l10n/gen/app_localizations.dart';
+import 'package:azteron_case/core/locale/locale_cubit.dart';
 import 'package:azteron_case/core/navigation/navigation_manager.dart';
 import 'package:azteron_case/core/theme/dark/dark_theme.dart';
 import 'package:azteron_case/core/theme/light/light_theme.dart';
@@ -15,12 +16,23 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) {
-        final cubit = ThemeCubit();
-        unawaited(cubit.init());
-        return cubit;
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) {
+            final cubit = ThemeCubit();
+            unawaited(cubit.init());
+            return cubit;
+          },
+        ),
+        BlocProvider(
+          create: (_) {
+            final cubit = LocaleCubit();
+            unawaited(cubit.init());
+            return cubit;
+          },
+        ),
+      ],
       child: const _AppView(),
     );
   }
@@ -32,21 +44,26 @@ class _AppView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ThemeCubit, ThemeState>(
-      builder: (context, state) {
-        return MaterialApp.router(
-          debugShowCheckedModeBanner: false,
-          title: 'Azteron Messages',
-          theme: LightTheme().theme,
-          darkTheme: DarkTheme().theme,
-          themeMode: state.flutterThemeMode,
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: AppLocalizations.supportedLocales,
-          routerConfig: NavigationManager().config,
+      builder: (context, themeState) {
+        return BlocBuilder<LocaleCubit, LocaleState>(
+          builder: (context, localeState) {
+            return MaterialApp.router(
+              debugShowCheckedModeBanner: false,
+              title: 'Azteron Messages',
+              theme: LightTheme().theme,
+              darkTheme: DarkTheme().theme,
+              themeMode: themeState.flutterThemeMode,
+              locale: localeState.locale,
+              localizationsDelegates: const [
+                AppLocalizations.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              supportedLocales: AppLocalizations.supportedLocales,
+              routerConfig: NavigationManager().config,
+            );
+          },
         );
       },
     );
